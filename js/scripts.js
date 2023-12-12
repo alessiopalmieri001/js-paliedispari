7e1a1a64d7c206f4d120a1f590c6ed949d2ff9ce
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2023-12-12 16:56:20 +0100

    palidroma

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,28 @@
+function palidromo(parola){
+
+    let lunghezza = parola.length;
+    
+    for (i = 0; i < lunghezza; i++){
+        
+        if (parola[i] != parola[lunghezza - 1 - i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
 const parolaPalindroma = prompt('inserire una parola palindroma');
 console.log(parolaPalindroma);
+
+
+if(palidromo(parolaPalindroma)){
+
+    alert('è palindromo');
+
+}
+
+else{
+    alert('non è palindroma')
+}
